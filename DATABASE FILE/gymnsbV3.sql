3c4d28a95f95283095ca101c27b15b06426a4cd4
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-09-01 00:40:43 -0500

    Create gymnsbV3.sql

---
+++ DATABASE FILE/gymnsbV3.sql
@@ -0,0 +1,409 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-09-2024 a las 07:40:03
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gymnsb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
+(2, 'admin', 'f2d0ff370380124029c2b807a924156c', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `announcements`
+--
+
+INSERT INTO `announcements` (`id`, `message`, `date`) VALUES
+(7, 'This is to announce that our GYM will remain close for 51 days due to COVID-19.', '2020-03-30'),
+(8, 'Opening of GYM Halls and Clubs are not fixed yet. Stay tuned for more updates!!', '2020-04-03'),
+(9, 'Renovation Going On...', '2020-04-04'),
+(10, 'This is a demo announcement from admin', '2022-06-03');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `curr_date` text NOT NULL,
+  `curr_time` text NOT NULL,
+  `present` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `vendor` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `equipment`
+--
+
+INSERT INTO `equipment` (`id`, `name`, `amount`, `quantity`, `vendor`, `description`, `address`, `contact`, `date`) VALUES
+(3, 'Treadmill', 909, 4, 'DnS', 'Edited Description', '7 Cedarstone Drive', '8521479633', '2019-03-07'),
+(4, 'Vertical Press Machine', 949, 3, 'SS Industries', 'For Biceps And Triceps, Upper Back, Chest', '7 Cedarstone Drive', '1245558980', '2020-03-19'),
+(5, 'Dumbell - Adjustable', 102, 26, 'Uptown Suppliers', 'Material: Steel, Rubber Plastic, Concrete', '7 Cedarstone Drive', '9875552100', '2020-03-29'),
+(6, 'Multi Bench Press Machine', 219, 2, 'DnS Suppliers', '6 In 1 Multi Bench With Incline, Flat, Decline Ben', '7 Cedarstone Drive', '7410001010', '2020-04-05'),
+(11, 'PRENSA', 600, 1, 'VENDEDOR DE PRENSAS', 'PRENSA AMARILLA', 'AMBATO', '8989898989', '2024-08-09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `members`
+--
+
+CREATE TABLE `members` (
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `dor` date NOT NULL,
+  `services` varchar(50) NOT NULL,
+  `amount` int(100) NOT NULL,
+  `paid_date` date NOT NULL,
+  `p_year` int(11) NOT NULL,
+  `plan` varchar(100) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `contact` varchar(10) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Active',
+  `attendance_count` int(100) NOT NULL,
+  `ini_weight` int(100) NOT NULL DEFAULT 0,
+  `curr_weight` int(100) NOT NULL DEFAULT 0,
+  `ini_bodytype` varchar(50) NOT NULL,
+  `curr_bodytype` varchar(50) NOT NULL,
+  `progress_date` date NOT NULL,
+  `reminder` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `members`
+--
+
+INSERT INTO `members` (`user_id`, `fullname`, `username`, `password`, `gender`, `dor`, `services`, `amount`, `paid_date`, `p_year`, `plan`, `address`, `contact`, `status`, `attendance_count`, `ini_weight`, `curr_weight`, `ini_bodytype`, `curr_bodytype`, `progress_date`, `reminder`) VALUES
+(36, 'VELASTEGUI CORDOVA MIGUEL EDUARDO', '0502020399', 'd41d8cd98f00b204e9800998ecf8427e', 'Masculino', '2024-08-02', '', 0, '2024-08-19', 2024, 'Fitness 2', 'uio', '8888', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
+(83, 'BARRENO VELASTEGUI ALEX ANTONIO', '0503243271', 'd41d8cd98f00b204e9800998ecf8427e', 'Masculino', '2024-08-16', '', 6, '2024-08-16', 2024, 'Rumba Fitness', 'SALCEDO', '9999999', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
+(85, 'HEREDIA SANDOVAL ANGELA DE LAS MERCEDES', '0502150956', 'd41d8cd98f00b204e9800998ecf8427e', 'Femenino', '2024-08-16', '', 6, '2024-08-17', 2024, 'Rumba', 'puyo', '888889', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
+(130, 'VELASTEGUI HEREDIA MIGUEL ANGEL', '1600596249', 'd41d8cd98f00b204e9800998ecf8427e', 'Masculino', '2024-08-31', '', 6, '2024-08-31', 2024, 'Fitness', 'PUYO', '0998912139', 'Active', 0, 0, 0, '', '', '0000-00-00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `id` int(11) NOT NULL,
+  `id_plan` int(255) NOT NULL,
+  `valor` varchar(50) NOT NULL,
+  `id_user` int(255) NOT NULL,
+  `fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pagos`
+--
+
+INSERT INTO `pagos` (`id`, `id_plan`, `valor`, `id_user`, `fecha`) VALUES
+(1, 2, '20', 79, '2024-08-16'),
+(2, 3, '40', 79, '2024-08-01'),
+(3, 3, '40', 36, '2024-07-10'),
+(4, 1, '35', 79, '2024-09-12'),
+(5, 3, '40', 36, '2024-06-06'),
+(9, 1, '35', 79, '2024-08-17'),
+(10, 3, '40', 36, '2024-08-19'),
+(11, 3, '40', 79, '2024-08-20'),
+(12, 0, '2.5', 0, '2024-09-01'),
+(13, 0, '2.5', 0, '2024-09-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rates`
+--
+
+CREATE TABLE `rates` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `charge` varchar(255) NOT NULL,
+  `timepo` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `rates`
+--
+
+INSERT INTO `rates` (`id`, `name`, `charge`, `timepo`) VALUES
+(1, 'Fitness', '35', '1'),
+(2, 'Rumba', '20', '1'),
+(3, 'Fitness 2', '40', '2'),
+(4, 'Rumba Fitness', '40', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reminder`
+--
+
+CREATE TABLE `reminder` (
+  `id` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `status` text NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `reminder`
+--
+
+INSERT INTO `reminder` (`id`, `name`, `message`, `status`, `date`, `user_id`) VALUES
+(12, 'staff', 'asd', 'unread', '2020-04-16 22:39:59', 0),
+(13, 'staff', 'asdasdas', 'unread', '2020-04-16 22:40:49', 0),
+(14, 'staff', 'ASasA', 'unread', '2020-04-16 22:41:59', 0),
+(15, 'staff', 'asdasdasd', 'unread', '2020-04-16 22:42:28', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `staffs`
+--
+
+CREATE TABLE `staffs` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `address` varchar(20) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `contact` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `staffs`
+--
+
+INSERT INTO `staffs` (`user_id`, `username`, `password`, `email`, `fullname`, `address`, `designation`, `gender`, `contact`) VALUES
+(5, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com', 'admin', 'admin', 'Manager', 'Male', 999999);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `task_status` varchar(50) NOT NULL,
+  `task_desc` varchar(30) NOT NULL,
+  `user_id` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `todo`
+--
+
+INSERT INTO `todo` (`id`, `task_status`, `task_desc`, `user_id`) VALUES
+(20, 'In Progress', 'Test Completed', 14),
+(21, 'Pending', 'Mastering Crunches', 6),
+(22, 'In Progress', 'Standing Workouts For Flat Abs', 6),
+(23, 'In Progress', 'Triceps Buildup - 3 set', 14),
+(24, 'Pending', 'Decline dumbbell bench press', 6),
+(27, 'Pending', 'dddd', 0),
+(28, 'In Progress', 'Test 1', 23);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indices de la tabla `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `rates`
+--
+ALTER TABLE `rates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reminder`
+--
+ALTER TABLE `reminder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `staffs`
+--
+ALTER TABLE `staffs`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indices de la tabla `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT de la tabla `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `members`
+--
+ALTER TABLE `members`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `rates`
+--
+ALTER TABLE `rates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `reminder`
+--
+ALTER TABLE `reminder`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `staffs`
+--
+ALTER TABLE `staffs`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
