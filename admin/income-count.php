26f475ccfca77136f27b39cd696d3463bfb92da7
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-09-14 10:51:03 -0500

    Update income-count.php

--- admin/income-count.php
+++ admin/income-count.php
@@ -10,8 +10,8 @@ $conn=mysqli_connect($servername,$uname,$pass,$db);
 if(!$conn){
     die("Connection Failed");
 }
-
-$sql = "SELECT SUM( valor) FROM pagos";
+    $fecha = date('Y-m-d');
+    $sql = "SELECT SUM( valor) FROM pagos WHERE fecha = '$fecha'";
         $amountsum = mysqli_query($conn, $sql) or die(mysqli_error($sql));
         $row_amountsum = mysqli_fetch_assoc($amountsum);
         $totalRows_amountsum = mysqli_num_rows($amountsum);
